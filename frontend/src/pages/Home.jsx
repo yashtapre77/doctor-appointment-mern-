aa4b0349368370edd7383f0c38cfa693d03a9e7c
yashtapre77 <yashtapre77@gmail.com>
2024-11-13 10:35:37 +0530

    imported Header component

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import Navbar from '../components/Navbar'
+import SpecialityMenu from '../components/SpecialityMenu'
+import Header from '../components/Header'
 
 function Home() {
   return (
     <div>
-      
+      <Header />
+      <SpecialityMenu />
     </div>
   )
 }
