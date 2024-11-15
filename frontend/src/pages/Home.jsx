a96b75d50d173edf4226aae14a77bd5e395ad656
yashtapre77 <yashtapre77@gmail.com>
2024-11-15 13:39:37 +0530

    small updations

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import Navbar from '../components/Navbar'
 import SpecialityMenu from '../components/SpecialityMenu'
 import Header from '../components/Header'
+import TopDoctors from '../components/TopDoctors'
 
 function Home() {
   return (
     <div>
       <Header />
       <SpecialityMenu />
+      <TopDoctors />
     </div>
   )
 }
