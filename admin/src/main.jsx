bbb5422c0c6dbd8a27299e8e4af22549ee6776dd
yashtapre77 <yashtapre77@gmail.com>
2024-12-23 11:16:09 +0530

    added browser router and contexprovider for admin

--- admin/src/main.jsx
+++ admin/src/main.jsx
@@ -1,10 +1,23 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import App from "./App.jsx";
+import "./index.css";
+import AdminContextProvider from "./context/AdminContext.jsx";
+import DoctorContextProvider from "./context/DoctorContext.jsx";
+import AppContextProvider from "./context/AppContext.jsx";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <BrowserRouter>
+      <AdminContextProvider>
+        <DoctorContextProvider>
+          <AppContextProvider>
+            <App />
+          </AppContextProvider>
+        </DoctorContextProvider>
+      </AdminContextProvider>
+    </BrowserRouter>
+    ,
+  </StrictMode>
+);
