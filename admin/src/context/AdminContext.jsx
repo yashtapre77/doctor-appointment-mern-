36fbade1ae1391568177478acdd5ffc4994a332b
yashtapre77 <yashtapre77@gmail.com>
2024-12-29 11:16:09 +0530

    updated admin context

--- admin/src/context/AdminContext.jsx
+++ admin/src/context/AdminContext.jsx
@@ -8,6 +8,8 @@ const AdminContextProvider = (props) => {
 
   const [aToken, setAToken] = useState(localStorage.getItem('aToken') ? localStorage.getItem('aToken') : '')
   const [doctors, setDoctors] = useState([])
+  const [appointments, setAppointments] = useState([])
+  const [dashData, setDashData] = useState(false)
 
   const backendUrl = import.meta.env.VITE_BACKEND_URL
 
@@ -46,11 +48,72 @@ const AdminContextProvider = (props) => {
 
   }
 
+  const getAllAppointments = async () => {
+
+    try {
+
+      const { data } = await axios.get(backendUrl, '/api/admin/appointments', { headers: { aToken } })
+
+      if (data.success) {
+        setAppointments(data.appointments)
+        console.log(data.appointments)
+      } else {
+        toast.error(data.message)
+      }
+
+    } catch (error) {
+      toast.error(error.message)
+    }
+
+  }
+
+  const cancelAppointment = async (appointmentId) => {
+
+    try {
+
+      const { data } = await axios.post(backendUrl + '/api/admin/cancel-appointment', { appointmentId }, { headers: { aToken } })
+
+      if (data.success) {
+        toast.success(data.message)
+        getAllAppointments()
+      } else {
+        toast.error(data.message)
+      }
+
+    } catch (error) {
+      toast.error(error.message)
+    }
+
+  }
+
+  const getDashData = async () => {
+
+    try {
+
+      const { data } = await axios.get(backendUrl + '/api/admin/dashboard', { headers: { aToken } })
+
+      if (data.success) {
+        setDashData(data.dashData)
+        console.log(data.dashData)
+      } else {
+        toast.error(data.message)
+      }
+
+    } catch (error) {
+      toast.error(data.message)
+    }
+
+  }
+
   const value = {
     aToken, setAToken,
     backendUrl,
     doctors, getAllDoctors,
-    changeAvailability
+    changeAvailability,
+    appointments, setAppointments,
+    getAllAppointments,
+    cancelAppointment,
+    dashData, getDashData
   }
 
   return (
