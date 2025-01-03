462e9cacae4fd09e5db9e3a9bb52ced375e0d031
yashtapre77 <yashtapre77@gmail.com>
2025-01-03 16:12:11 +0530

    controller to book appointment

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -124,4 +124,58 @@ const getProfile = async (req, res) => {
     }
   
   }
+
+// API to book appointment
+const bookAppointment = async (req, res) => {
+
+    try {
+  
+      const { userId, docId, slotDate, slotTime } = req.body
+  
+      const docData = await doctorModel.findById(docId).select('-password')
+  
+      if (!docData.available) {
+        return res.json({ success: false, message: 'Doctor not available' })
+      }
+  
+      let slots_booked = docData.slots_booked
+  
+      // checking for slot availablity
+      if (slots_booked[slotDate]) {
+        if (slots_booked[slotDate].includes(slotTime)) {
+          return res.json({ success: false, message: 'Slot not available' })
+        } else {
+          slots_booked[slotDate].push(slotTime)
+        }
+      } else {
+        slots_booked[slotDate] = []
+        slots_booked[slotDate].push(slotTime)
+      }
+  
+      const userData = await userModel.findById(userId).select('-password')
+  
+      delete docData.slots_booked
+  
+      const appointmentData = {
+        userId, docId,
+        userData, docData,
+        amount: docData.fees,
+        slotTime, slotDate,
+        date: Date.now()
+      }
+  
+      const newAppointment = new appointmentModel(appointmentData)
+      await newAppointment.save()
+  
+      // save new slots data in docData
+      await doctorModel.findByIdAndUpdate(docId, { slots_booked })
+  
+      res.json({ success: true, message: 'Appointment Booked' })
+  
+    } catch (error) {
+      console.log(error)
+      res.json({ success: false, message: error.message })
+    }
+  
+  }
   
