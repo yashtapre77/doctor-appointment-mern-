be90c801e4893747d255bb43f831e481e62dc430
yashtapre77 <yashtapre77@gmail.com>
2025-01-03 13:26:31 +0530

    controller to fetch user data

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -75,3 +75,53 @@ const loginUser = async (req, res) => {
   }
 
 }
+
+// API to get user profile data
+const getProfile = async (req, res) => {
+
+    try {
+  
+      const { userId } = req.body
+      const userData = await userModel.findById(userId).select('-password')
+  
+      res.json({ success: true, userData })
+  
+    } catch (error) {
+      console.log(error)
+      res.json({ success: false, message: error.message })
+    }
+  
+  }
+  
+  // API to update user profile
+  const updateProfile = async (req, res) => {
+  
+    try {
+  
+      const { userId, name, phone, address, dob, gender } = req.body
+      const imageFile = req.file
+  
+      if (!name || !phone || !dob || !gender) {
+        return res.json({ success: false, message: 'Data Missing' })
+      }
+  
+      await userModel.findByIdAndUpdate(userId, { name, phone, address: JSON.parse(address), dob, gender })
+  
+      if (imageFile) {
+  
+        // upload image to cloudinary
+        const imageUpload = await cloudinary.uploader.upload(imageFile.path, { resource_type: 'image' })
+        const imageURL = imageUpload.secure_url
+  
+        await userModel.findByIdAndUpdate(userId, { image: imageURL })
+      }
+  
+      res.json({ success: true, message: 'Profile Updated' })
+  
+    } catch (error) {
+      console.log(error)
+      res.json({ success: false, message: error.message })
+    }
+  
+  }
+  
